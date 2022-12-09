9550920e2c1a5f9b72b39b697238668f443fe1ec
Antoniu Ficard <39056361+killedBy11@users.noreply.github.com>
2022-12-10 01:38:32 +0200

    Added map styling

--- src/MyMap.js
+++ src/MyMap.js
@@ -5,6 +5,111 @@ import "./Map.css"
 const positions = [{lat: 44, lng: -80}, {lat: 45, lng: -90}]
 
 export default function MyMap(pos) {
+    const options = {
+        disableDefaultUI: true,
+        styles: [
+            {
+              "featureType": "administrative.country",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#e6f737"
+                }
+              ]
+            },
+            {
+              "featureType": "landscape.man_made",
+              "elementType": "geometry.fill",
+              "stylers": [
+                {
+                  "color": "#edfcfc"
+                }
+              ]
+            },
+            {
+              "featureType": "poi.park",
+              "elementType": "geometry.fill",
+              "stylers": [
+                {
+                  "color": "#ade3a6"
+                }
+              ]
+            },
+            {
+              "featureType": "poi.sports_complex",
+              "elementType": "geometry.fill",
+              "stylers": [
+                {
+                  "color": "#a1e19e"
+                }
+              ]
+            },
+            {
+              "featureType": "road.arterial",
+              "elementType": "geometry.fill",
+              "stylers": [
+                {
+                  "color": "#f0f1ce"
+                }
+              ]
+            },
+            {
+              "featureType": "road.arterial",
+              "elementType": "geometry.stroke",
+              "stylers": [
+                {
+                  "color": "#656147"
+                }
+              ]
+            },
+            {
+              "featureType": "road.highway",
+              "elementType": "geometry.fill",
+              "stylers": [
+                {
+                  "color": "#dd62bc"
+                }
+              ]
+            },
+            {
+              "featureType": "road.highway",
+              "elementType": "geometry.stroke",
+              "stylers": [
+                {
+                  "color": "#4f2644"
+                }
+              ]
+            },
+            {
+              "featureType": "road.local",
+              "elementType": "geometry.fill",
+              "stylers": [
+                {
+                  "color": "#e9c1be"
+                }
+              ]
+            },
+            {
+              "featureType": "road.local",
+              "elementType": "geometry.stroke",
+              "stylers": [
+                {
+                  "color": "#a28282"
+                }
+              ]
+            },
+            {
+              "featureType": "water",
+              "elementType": "geometry.fill",
+              "stylers": [
+                {
+                  "color": "#a3aaec"
+                }
+              ]
+            }
+          ]
+    };
+
     const pct = useMemo(() => ({lat: 44, lng: -80}), [])
 
     const {isLoaded} = useLoadScript({
@@ -19,7 +124,7 @@ export default function MyMap(pos) {
         return <div>Loading...</div>
     }
     return (
-        <GoogleMap zoom={10} center={pct} mapContainerClassName={"map-container"} >
+        <GoogleMap zoom={10} center={pct} mapContainerClassName={"map-container"} options={options}>
             {markers}
         </GoogleMap>
     )
